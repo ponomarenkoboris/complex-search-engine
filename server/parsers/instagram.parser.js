5d7aa55a2b45c6d421c278a29743410164632bfa
Boris <65194683+ponomarenkoboris@users.noreply.github.com>
2021-02-05 22:13:21 +0300

    Update instagram.parser.js

--- server/parsers/instagram.parser.js
+++ server/parsers/instagram.parser.js
@@ -1,4 +1,3 @@
-// <meta property="og:description" content="216 подписчиков, 191 подписок, 8 публикаций — посмотрите в Instagram фото и видео 𝔅𝔬𝔯𝔦𝔰 𝔇𝔪𝔦𝔱𝔯𝔦𝔢𝔳𝔦𝔠𝔥 (@_twisted_m.e.n.t.a.l)" /> вытащить это значение 
 //<meta property="og:image" content="https://scontent-hel3-1.cdninstagram.com/v/t51.2885-19/s150x150/119192934_748483762382635_5962425138854134302_n.jpg?_nc_ht=scontent-hel3-1.cdninstagram.com&_nc_ohc=ms4f3aFSRYAAX8_76B_&tp=1&oh=62e235c9f538eb638c3e4f54469194ee&oe=60284B1F" />
 // const regExpUsername = /\(@\w+.+\)/; for instagram
 // const regExpFollowers = /<meta property="og:description" content="\d+/;
